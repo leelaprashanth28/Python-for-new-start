3c6302edf137112f81cb575528987f26397fa15c
leelaprashanth28 <leelaprashanth28@gmail.com>
2026-01-04 19:23:12 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# DAY-1:
+  
+#   Running Comments, Escape Sequences and Print Statements
+
+# Comments can be implemented using these symbols (# , ''' ''' , """ """) where:
+
+# (#) can be used for making single line comment
+''' (''' ''' or """ """) can be used for for para comments '''
+
+'''Escape Sequences are the characters used in print statements following the character "\" which are not printed
+but can perform specific output operations. Basic Escape Sequences in python are \n for next line, \t for tab space,
+ \\ for backslash, \" for quotes'''
+
+print("Hey, This is my Day 1\n And I'm Executing Comments, Escape Sequences and Print Statements")
+
+'''The print() statements are used for displaying the content in the output screen'''
+
+print("Hey, this is the output for the print statement", 3, 7, sep=",\n")
