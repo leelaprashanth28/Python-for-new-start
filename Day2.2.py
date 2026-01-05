707ffcc31d6f7bd7e908286f6f70ff9d3f112eb6
leelaprashanth28 <leelaprashanth28@gmail.com>
2026-01-05 15:34:18 +0530

    Add files via upload
    Program for Arithmetic operators

---
+++ Day2.2.py
@@ -0,0 +1,8 @@
+'''Arithmetic Operator for performing calculations and Building a Static Calculator'''
+''' +, -, *, /, //, %, ** are used for performing addition, subtraction, multiplication, division, round fig division,
+modular division, power'''
+
+a = 10
+b = 3
+print("Sum: ", a+b,"Difference: ", a-b,"Product: ", a*b,"Quotient: ", a/b,"Round Fig Difference: ", a//b, sep = "\n")
+print("Remainder: ", a%b,"Power Result: ", a**b, sep = "\n")
